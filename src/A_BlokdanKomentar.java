cc04db84d31e598066b408809f08e429221eb3b7
LENOVO <saptoaman86@gmail.com>
2022-03-17 16:14:43 +0700

    P1.DasarJava

---
+++ src/A_BlokdanKomentar.java
@@ -0,0 +1,14 @@
+public class A_BlokdanKomentar {
+    public static void main(String[] args) {
+        System.out.println("Pemrograman Berorientasi Objek Dengan Java ");
+        //contoh komentar satu baris
+
+        /*
+        contoh komentar lebih dari satu baris
+         */
+
+        /*
+         *java Doc -->baris ini untuk mendokumentasikan kode program
+         */
+    }
+}
---
+++ src/W_OutputPrintF.java
@@ -0,0 +1,5 @@
+public class W_OutputPrintF {
+    public static void main(String[] args) {
+        
+    }
+}
